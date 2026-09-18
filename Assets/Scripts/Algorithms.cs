f3f9c849a3aaf6b03fe86268d4f55eeaf8c0a467
linthiee <eluujazmin@gmail.com>
2026-09-18 02:20:19 -0300

    feat: add class skeleton with all the methods to implement

---
+++ Assets/Scripts/Algorithms.cs
@@ -0,0 +1,65 @@
+using System;
+
+public static class Algorithms 
+{
+   public static void BitonicSort<T>(T[] array) where T : IComparable<T>
+   {
+      throw new System.NotImplementedException();
+   }
+   public static void SelectionSort<T>(T[] array) where T : IComparable<T>
+   {
+      throw new System.NotImplementedException();
+   }
+   public static void CocktailShakerSort<T>(T[] array) where T : IComparable<T>
+   {
+      throw new System.NotImplementedException();
+   }
+   public static void QuickSort<T>(T[] array) where T : IComparable<T>
+   {
+      throw new System.NotImplementedException();
+   }
+   public static void RadixSort_LSD<T>(T[] array) where T : IComparable<T>
+   {
+      throw new System.NotImplementedException();
+   }
+   public static void ShellSort<T>(T[] array) where T : IComparable<T>
+   {
+      throw new System.NotImplementedException();
+   }
+   public static void BogoSort<T>(T[] array) where T : IComparable<T>
+   {
+      throw new System.NotImplementedException();
+   }
+   public static void RadixSort_MSD<T>(T[] array) where T : IComparable<T>
+   {
+      throw new System.NotImplementedException();
+   }
+   public static void IntroSort<T>(T[] array) where T : IComparable<T>
+   {
+      throw new System.NotImplementedException();
+   }
+   public static void AdaptiveMergeSort<T>(T[] array) where T : IComparable<T>
+   {
+      throw new System.NotImplementedException();
+   }
+   public static void BubbleSort<T>(T[] array) where T : IComparable<T>
+   {
+      throw new System.NotImplementedException();
+   }
+   public static void GnomeSort<T>(T[] array) where T : IComparable<T>
+   {
+      throw new System.NotImplementedException();
+   }
+   public static void MergeSort<T>(T[] array) where T : IComparable<T>
+   {
+      throw new System.NotImplementedException();
+   }
+   public static void HeapSort<T>(T[] array) where T : IComparable<T>
+   {
+      throw new System.NotImplementedException();
+   }
+   public static void InsertionSort<T>(T[] array) where T : IComparable<T>
+   {
+      throw new System.NotImplementedException();
+   }
+}
